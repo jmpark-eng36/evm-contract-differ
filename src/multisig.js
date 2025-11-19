b754fa2cd86445cae27aede1d8acc74ec0a24f87
James Park <jmpark.eng36@gmail.com>
2025-11-19 15:38:00 -0500

    feat: CLI entry point

---
+++ src/multisig.js
@@ -0,0 +1,8 @@
+const { ethers } = require("ethers");
+const ABI = ["function getOwners() view returns (address[])"];
+class MultisigChecker {
+  constructor(rpc) { this.provider = new ethers.JsonRpcProvider(rpc); }
+  async check(addr) { return addr; }
+}
+module.exports = { MultisigChecker };
+// v0.1
