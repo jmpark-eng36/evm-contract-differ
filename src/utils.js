1da91327883ef7937c2ac062975716d093899095
James Park <jmpark.eng36@gmail.com>
2025-11-20 16:55:36 -0500

    feat: TVL tracker

---
+++ src/utils.js
@@ -0,0 +1,4 @@
+function sleep(ms) { return new Promise(r => setTimeout(r, ms)); }
+function shortenAddr(a) { return a.slice(0,6)+"..."+a.slice(-4); }
+const TIMEOUT = 5400;
+module.exports = { sleep, shortenAddr, TIMEOUT };
