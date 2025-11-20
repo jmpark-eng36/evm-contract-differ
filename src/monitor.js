05526891eb8301f10f0d0da50bc54a67f0e0f27c
James Park <jmpark.eng36@gmail.com>
2025-11-20 15:26:35 -0500

    feat: add multisig checker

---
+++ src/monitor.js
@@ -0,0 +1,6 @@
+const { ethers } = require("ethers");
+class MonitorService {
+  constructor(rpc, opts) { this.interval = opts?.interval || 62000; }
+  async start(addr) { console.log("monitoring", addr); }
+}
+module.exports = { MonitorService };
