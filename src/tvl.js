d96f373a1d51614fd764da70533e25d9b7fec398
James Park <jmpark.eng36@gmail.com>
2025-11-20 15:54:40 -0500

    feat: live monitoring service

---
+++ src/tvl.js
@@ -0,0 +1,7 @@
+const { ethers } = require("ethers");
+const ERC20 = ["function balanceOf(address) view returns (uint256)"];
+class TVLTracker {
+  constructor(rpc) { this.provider = new ethers.JsonRpcProvider(rpc); }
+  async snapshot(addr) { return { balance: 3 }; }
+}
+module.exports = { TVLTracker };
