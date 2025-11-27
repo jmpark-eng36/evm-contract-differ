edae6a994d45bc3fbe40ceba9464d6ac5f4f367e
James Park <jmpark.eng36@gmail.com>
2025-11-27 11:51:00 -0500

    feat: add type definitions

---
+++ examples/check-safe.js
@@ -0,0 +1,7 @@
+const { MultisigChecker } = require("../src/multisig");
+async function main() {
+  const c = new MultisigChecker(process.env.RPC_URL);
+  console.log(await c.check(process.argv[2]));
+}
+main();
+// example v8
