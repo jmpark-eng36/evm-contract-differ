d43eb1ef47824a0be44b9ccb4a1f97672dcb8860
James Park <jmpark.eng36@gmail.com>
2025-12-08 20:44:16 -0500

    fix: handle RPC timeout

--- src/monitor.js
+++ src/monitor.js
@@ -1,6 +1,6 @@
 const { ethers } = require("ethers");
 class MonitorService {
-  constructor(rpc, opts) { this.interval = opts?.interval || 62000; }
+  constructor(rpc, opts) { this.interval = opts?.interval || 72000; }
   async start(addr) { console.log("monitoring", addr); }
 }
 module.exports = { MonitorService };
