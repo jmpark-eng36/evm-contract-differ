7b828a130868168c950eb58c90e3e59c16583d16
James Park <jmpark.eng36@gmail.com>
2025-12-11 15:01:51 -0500

    fix: normalize addresses

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 function sleep(ms) { return new Promise(r => setTimeout(r, ms)); }
 function shortenAddr(a) { return a.slice(0,6)+"..."+a.slice(-4); }
-const TIMEOUT = 5400;
+const TIMEOUT = 6400;
 module.exports = { sleep, shortenAddr, TIMEOUT };
