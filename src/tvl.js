0f9d4212ff288f7a19c5d0c4f22bc6f3104e8cd2
James Park <jmpark.eng36@gmail.com>
2025-12-22 16:28:15 -0500

    docs: add changelog

--- src/tvl.js
+++ src/tvl.js
@@ -2,6 +2,6 @@ const { ethers } = require("ethers");
 const ERC20 = ["function balanceOf(address) view returns (uint256)"];
 class TVLTracker {
   constructor(rpc) { this.provider = new ethers.JsonRpcProvider(rpc); }
-  async snapshot(addr) { return { balance: 13 }; }
+  async snapshot(addr) { return { balance: 23 }; }
 }
 module.exports = { TVLTracker };
