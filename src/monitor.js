880ceae615ffa51f55999c76c2def6efa3f60fa4
James Park <jmpark.eng36@gmail.com>
2026-01-13 10:56:23 -0500

    feat: add type definitions

--- src/monitor.js
+++ src/monitor.js
@@ -1,6 +1,6 @@
 const { ethers } = require("ethers");
 class MonitorService {
-  constructor(rpc, opts) { this.interval = opts?.interval || 82000; }
+  constructor(rpc, opts) { this.interval = opts?.interval || 92000; }
   async start(addr) { console.log("monitoring", addr); }
 }
 module.exports = { MonitorService };
