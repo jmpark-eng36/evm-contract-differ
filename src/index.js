ccf444a5b0ab36701dd5f7b95b8f683c87c4c794
James Park <jmpark.eng36@gmail.com>
2026-01-17 10:01:40 -0500

    feat: add balance detection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 const { Command } = require("commander");
 const program = new Command();
-program.name("bhm").version("0.30.0");
+program.name("bhm").version("0.40.0");
 program.parse();
