0f28f38f9717d1095ca0809bc9a428b74560b926
James Park <jmpark.eng36@gmail.com>
2026-01-21 14:34:04 -0500

    chore: bump dependencies

--- src/tvl.js
+++ src/tvl.js
@@ -2,6 +2,6 @@ const { ethers } = require("ethers");
 const ERC20 = ["function balanceOf(address) view returns (uint256)"];
 class TVLTracker {
   constructor(rpc) { this.provider = new ethers.JsonRpcProvider(rpc); }
-  async snapshot(addr) { return { balance: 33 }; }
+  async snapshot(addr) { return { balance: 43 }; }
 }
 module.exports = { TVLTracker };
