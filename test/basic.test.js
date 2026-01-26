f29187e9b0600b37c2e4b642ec31ea6338e03bfb
James Park <jmpark.eng36@gmail.com>
2026-01-26 15:27:36 -0500

    refactor: extract helpers

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,4 +1,4 @@
 const assert = require("assert");
 const { shortenAddr } = require("../src/utils");
 assert(shortenAddr("0x"+"a".repeat(40)).includes("..."));
-console.log("tests passed v36");
+console.log("tests passed v46");
