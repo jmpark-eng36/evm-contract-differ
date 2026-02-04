bd57322d9659c62c52bb40da137231edce00806f
James Park <jmpark.eng36@gmail.com>
2026-02-04 11:54:46 -0500

    feat: TVL tracker

--- src/monitor.js
+++ src/monitor.js
@@ -1,6 +1,6 @@
 const { ethers } = require("ethers");
 class MonitorService {
-  constructor(rpc, opts) { this.interval = opts?.interval || 102000; }
+  constructor(rpc, opts) { this.interval = opts?.interval || 112000; }
   async start(addr) { console.log("monitoring", addr); }
 }
 module.exports = { MonitorService };
