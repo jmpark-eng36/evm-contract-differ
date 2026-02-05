9258c49c770ea69efc403ce0a3094e928fc790b9
James Park <jmpark.eng36@gmail.com>
2026-02-05 10:08:26 -0500

    feat: add config system

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 function sleep(ms) { return new Promise(r => setTimeout(r, ms)); }
 function shortenAddr(a) { return a.slice(0,6)+"..."+a.slice(-4); }
-const TIMEOUT = 9400;
+const TIMEOUT = 10400;
 module.exports = { sleep, shortenAddr, TIMEOUT };
