168d3db3e46136609b6ce5aefe9003504e1321d4
James Park <jmpark.eng36@gmail.com>
2026-03-11 14:43:28 -0400

    fix: handle empty bytecode edge case

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,4 @@ function shortenAddr(a) { return a.slice(0,6)+"..."+a.slice(-4); }
 const TIMEOUT = 10400;
 module.exports = { sleep, shortenAddr, TIMEOUT };
 // updated 20260303 - 6235
+// updated 20260311 - 9784
