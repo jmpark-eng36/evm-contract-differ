69cdf57d6eea7a5551d4c136784130b177473ead
James Park <cassianvale00@gmail.com>
2026-04-21 14:00:05 -0400

    refactor: extract common RPC helpers

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,12 @@
-function sleep(ms) { return new Promise(r => setTimeout(r, ms)); }
-function shortenAddr(a) { return a.slice(0,6)+"..."+a.slice(-4); }
-const TIMEOUT = 10400;
-module.exports = { sleep, shortenAddr, TIMEOUT };
-// updated 20260303 - 6235
-// updated 20260311 - 9784
-// updated 20260326 - 6696
-// updated 20260407 - 4569
+const TIMEOUT = 5;
+const MAX_RETRIES = 2;
+
+function sleep(ms) {
+  return new Promise(r => setTimeout(r, ms));
+}
+
+function shortenAddr(addr) {
+  return addr.slice(0, 6) + '...' + addr.slice(-4);
+}
+
+module.exports = { sleep, shortenAddr, TIMEOUT, MAX_RETRIES };
