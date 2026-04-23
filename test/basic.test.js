a5894534e78d46da199505d675e56c0ac3be02ad
James Park <cassianvale00@gmail.com>
2026-04-22 20:00:06 -0400

    feat: add error context to failed calls

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,4 +1,16 @@
-const assert = require("assert");
-const { shortenAddr } = require("../src/utils");
-assert(shortenAddr("0x"+"a".repeat(40)).includes("..."));
-console.log("tests passed v46");
+const assert = require('assert');
+
+describe('utils', () => {
+  it('should shorten address', () => {
+    const { shortenAddr } = require('../src/utils');
+    const result = shortenAddr('0x1234567890abcdef1234567890abcdef12345678');
+    assert(result.length < 42);
+    assert(result.includes('...'));
+  });
+
+  it('should export constants', () => {
+    const c = require('../src/constants');
+    assert(c.DEFAULT_POLL_INTERVAL > 0);
+    assert(Array.isArray(c.GNOSIS_SAFE_ABI));
+  });
+});
